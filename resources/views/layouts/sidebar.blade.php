33f52cd929bac0f50bb1692b462365e5afa4275d
Louis <louis.zerri@hotmail.com>
2025-05-30 04:21:18 +0200

    Fix: Sidebar style

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="sidebar">
-    <div>
+    <div class="sidebar-content">
         <!-- Logo -->
         <div class="mb-5 text-center">
             <svg width="85" height="113" viewBox="0 0 85 113" fill="none" xmlns="http://www.w3.org/2000/svg">
